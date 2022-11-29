e488ef8b5be794a0ae98a22c194f11ff3e225268
Mariana Rodriguez <marianar@email.unc.edu>
2022-11-29 16:59:06 -0500

    made changes.

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,17 @@
+//command line interface should take three arguments with the following defaults
+// the number of sides of the die you are rolling (default to 6 sides)
+//the number of dice you are rolling (default to 2 dice).
+//the number of times the dice are rolled (default to 1 time).
+
 #!/usr/bin/env node	
 	
-import minimist from "minimist";
 import {roll} from "../lib/roll.js";
+import minimist from "minimist";
 	
-const args = minimist(process.argv.slice(2));
-	
-const sides = args.sides || 6;
-const dice = args.dice || 2;
-const rolls = args.roll || 1;
-	
+let args = minimist(process.argv.slice(2));
+
+let rolls = args.roll || 1;	
+let dice = args.dice || 2;
+let sides = args.sides || 6;
+
 console.log(JSON.stringify(roll(sides, dice, rolls)))
