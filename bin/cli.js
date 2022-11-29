ae2e884eab93cc082ef3a088975cdee3db79e107
Mariana Rodriguez <marianar@email.unc.edu>
2022-11-29 16:43:33 -0500

    Added log file.

--- bin/cli.js
+++ bin/cli.js
@@ -1,18 +1,14 @@
-#!/usr/bin/env node
+#!/usr/bin/env node	
+	
+import {roll} from "../lib/roll.js";
+	
+import minimist from "minimist";
+	
+const args = minimist(process.argv.slice(2));
+	
+const sides = args.sides || 6;
+const dice = args.dice || 2;
+const rolls = args.roll || 1;
+	
+console.log(JSON.stringify(roll(sides, dice, rolls)))
 
-
-
-import { roll } from "/lib/roll.js"
-import minimist from "minimist"
-
-let args = minimist(process.argv.slice(2))
-
-
-const rolls = args.rolls? args.rolls:1
-const sides = args.sides? args.sides:6
-const dice = args.dice? args.dice:2
-
-
-
-const user_output  = roll(sides, rolls, dice)
-console.log(JSON.stringify(user_output))
